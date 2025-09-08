70f44a8d1ba68a8fa71a147ec0309818443d4f9a
Durga Devi S <durga26604@gmail.com>
2025-09-08 19:06:25 +0530

    Update pattern28.py

--- pattern28.py
+++ pattern28.py
@@ -1,8 +1,8 @@
 print('------------')
 print(' Pattern-28 ')
 print('------------')
-n=int(input())
-for i in range(0,n):
+def pattern28(n):
+ for i in range(0,n):
     for j in range(n-1,i-1,-1):
         if i==0 or j==n-1 or i==j:
           print("*",end='')
@@ -11,4 +11,5 @@ for i in range(0,n):
     print()
 n=int(input())
 pattern28(n)
+
 
