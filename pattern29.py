615b991575e4e52f45e715745209ac5b6678b34c
Durga Devi S <durga26604@gmail.com>
2025-09-08 19:06:54 +0530

    Update pattern29.py

--- pattern29.py
+++ pattern29.py
@@ -1,15 +1,15 @@
 print('------------')
 print(' Pattern-29 ')
 print('------------')
-n=int(input())
-for i in range(0,n):
+def pattern29(n):
+ for i in range(0,n):
     for j in range(n-1,i-1,-1):
         if i==0 or i==j or j==n-1:
             print('*',end='')
         else:
             print(' ',end='')
     print()
-for i in range(0,n):
+ for i in range(0,n):
     for j in range(0,i+1):
         if i==0 or i==n-1 or j==i or j==0:
             print('*',end='')
@@ -18,4 +18,5 @@ for i in range(0,n):
     print()
 n=int(input())
 pattern29(n)
+
 
