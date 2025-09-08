f13bddaa1ddc75e6912637fa662197bbf3a721f5
Durga Devi S <durga26604@gmail.com>
2025-09-08 18:54:36 +0530

    Update pattern35.py

--- pattern35.py
+++ pattern35.py
@@ -1,10 +1,11 @@
 print('------------')
 print(' Pattern-35')
 print('------------')
-n=int(input())
-for i in range(n,0,-1):
+def pattern35(n):
+ for i in range(n,0,-1):
     for j in range(n,i-1,-1):
         print(chr(64+j),end='')
     print()
 n=int(input())
 pattern35(n)
+
