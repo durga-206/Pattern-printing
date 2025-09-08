f02e73084f869624aacf1ef3a4d42798fba0580e
Durga Devi S <durga26604@gmail.com>
2025-09-08 18:55:52 +0530

    Update pattern33.py

--- pattern33.py
+++ pattern33.py
@@ -1,11 +1,12 @@
 print('------------')
 print(' Pattern-33')
 print('------------')
-n=int(input())
-for i in range(0,n):
+def pattern33(n):
+ for i in range(0,n):
     for j in range(0,n-i):
         print(chr(n-i+64),end='')
     print()
 n=int(input())
 pattern33(n)
+
 
