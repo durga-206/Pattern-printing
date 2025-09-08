1229e00da62c0dbf9eaa9e7667d1314862c6e713
Durga Devi S <durga26604@gmail.com>
2025-09-08 19:04:50 +0530

    Update pattern25.py

--- pattern25.py
+++ pattern25.py
@@ -1,15 +1,16 @@
 print('------------')
 print(' Pattern-25 ')
 print('------------')
-n=int(input())
-for i in range(0,n):
+def pattern25(n):
+ for i in range(0,n):
     for j in range(0,i+1):
         print('*',end=' ')
     print()
-for i in range(0,n-1):
+ for i in range(0,n-1):
     for j in range(0,n-1-i):
         print('*',end=' ')
     print()
 n=int(input())
 pattern25(n)
+
 
