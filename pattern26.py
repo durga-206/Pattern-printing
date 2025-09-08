74836ee1850af935a2d3479c250e278392bf6595
Durga Devi S <durga26604@gmail.com>
2025-09-08 19:05:15 +0530

    Update pattern26.py

--- pattern26.py
+++ pattern26.py
@@ -1,14 +1,14 @@
 print('------------')
 print(' Pattern-26 ')
 print('------------')
-n=int(input())
-for i in range(0,n):
+def pattern25(n):
+ for i in range(0,n):
     for space in range(1,n-i):
         print(' ',end='')
     for star in range(0,i+1):
         print('*',end='')
     print()
-for i in range(0,n):
+ for i in range(0,n):
     for space in range(0,i+1):
         print(' ',end='')
     for j in range(0,n-i-1):
@@ -16,4 +16,5 @@ for i in range(0,n):
     print()
 n=int(input())
 pattern26(n)
+
 
